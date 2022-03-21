6902aff8e953536268a4d1d99e4be2fbfd4421ff
Emerson <emersonn956@gmail.com>
2022-03-21 20:54:05 -0300

    Criando projeto e adicionando dependencias

---
+++ src/main/java/com/estudosJava/api1/ProjetoApiApplication.java
@@ -0,0 +1,13 @@
+package com.estudosJava.api1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoApiApplication.class, args);
+	}
+
+}
