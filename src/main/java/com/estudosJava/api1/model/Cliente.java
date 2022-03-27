b16cc0c0219440de50caa799ef548486071d58b5
Emerson <emersonn956@gmail.com>
2022-03-27 11:53:10 -0300

    Adiconando anotações referenciando o BD

--- src/main/java/com/estudosJava/api1/model/Cliente.java
+++ src/main/java/com/estudosJava/api1/model/Cliente.java
@@ -1,6 +1,38 @@
 package com.estudosJava.api1.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+//mapeada com entity é tratada como entidade e logo vai para o BD, tabela cliente
+@Entity
 public class Cliente {
+	//@id = primary key @generate = da a responsabilidade de incremento ao BD
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@Column(nullable = false)
 	private String nome;
+	
+	/*GET e SET metodos de acessibilidade para as propriedades, ja que são privadas*/
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
 }
